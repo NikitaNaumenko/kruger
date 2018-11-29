eb77a5a32af2f0e6e954766041add10278463b82
Nikita Naumenko <naumenkoniki@gmail.com>
2018-11-29 21:10:00 +0300

    complete endpoints (#4)

--- lib/kruger/client/vacancies.rb
+++ lib/kruger/client/vacancies.rb
@@ -10,6 +10,22 @@ module Kruger
       def responses(vacancy_id:)
         get("vacancies/#{vacancy_id}/responses", query: { access_token: @access_token })
       end
+
+      def create_vacancy(options:)
+        post('/vacancies', options.merge(query: { access_token: @access_token }))
+      end
+
+      def update_vacancy(vacancy_id:, options:)
+        patch("vacancies/#{vacancy_id}", options.merge(query: { access_token: @access_token }))
+      end
+
+      def publish_vacancy(vacancy_id:)
+        put("vacancies/#{vacancy_id}/pay", query: { access_token: @access_token })
+      end
+
+      def hide_vacancy(vacancy_id:)
+        put("vacancies/#{vacancy_id}/toggle_published_state", query: { access_token: @access_token })
+      end
     end
   end
 end
